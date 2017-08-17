1f60460a10f774d1c3e6c86e8a68a0658064ef65
David Mandelbaum <dasmandelbaum@gmail.com>
2017-08-17 00:15:01 -0400

    first commit finances

---
+++ FinancesReview/financialSpending.js
@@ -0,0 +1,448 @@
+            var rent = {
+  				x: ['June', 'July', 'August', 'September', 'October', 'November', 
+  					'December', 'January', 'February', 'March', 'April', 'May'],
+  				y: [1100,1550,1550,1550,1550,1550,1550,1550,1550,1550,1550,1550],
+  				name: 'Rent',
+  				type: 'bar',
+  				marker: {
+    				color: 'A9BBF7'
+  				}
+			};
+			var rentRose = {
+  				t: ['June', 'July', 'August', 'September', 'October', 'November', 
+  					'December', 'January', 'February', 'March', 'April', 'May'],
+  				r: [1100,1550,1550,1550,1550,1550,1550,1550,1550,1550,1550,1550],
+  				name: 'Rent',
+  				type: 'area',
+  				marker: {
+    				color: 'A9BBF7'
+  				},
+  				textinfo: 'none'
+			};
+
+			var rentInsurance = {
+  				x: ['June', 'July', 'August', 'September', 'October', 'November', 
+  					'December', 'January', 'February', 'March', 'April', 'May'],
+  				y: [0,0,0,0,0,23.47,23.47,23.47,23.47,23.47,23.47,23.46],
+  				name: "Renter's Insurance",
+  				type: 'bar',
+  				marker: {
+  					color: 'A9E0F7'
+  				}
+			};
+			
+			var rentInsuranceRose = {
+  				t: ['June', 'July', 'August', 'September', 'October', 'November', 
+  					'December', 'January', 'February', 'March', 'April', 'May'],
+  				r: [0,0,0,0,0,23.47,23.47,23.47,23.47,23.47,23.47,23.46],
+  				name: "Renter's Insurance",
+  				type: 'area',
+  				marker: {
+  					color: 'A9E0F7'
+  				}
+			};
+			
+			var coned = {
+  				x: ['June', 'July', 'August', 'September', 'October', 'November', 
+  					'December', 'January', 'February', 'March', 'April', 'May'],
+  				y: [20.33,81.26,85.47,75.87,79.31,139.79,81.74,82.11,75.09,91.88,78.47,55.69],
+  				name: 'ConEd',
+  				type: 'bar',
+  				marker: {
+  					color: 'A9F7BE'
+  				}
+			};
+			
+			var conedRose = {
+  				t: ['June', 'July', 'August', 'September', 'October', 'November', 
+  					'December', 'January', 'February', 'March', 'April', 'May'],
+  				r: [20.33,81.26,85.47,75.87,79.31,139.79,81.74,82.11,75.09,91.88,78.47,55.69],
+  				name: 'ConEd',
+  				type: 'area',
+  				marker: {
+  					color: 'A9F7BE'
+  				}
+			};
+
+			var internet = {
+  				x: ['June', 'July', 'August', 'September', 'October', 'November', 
+  					'December', 'January', 'February', 'March', 'April', 'May'],
+  				y: [247.46,51.83,31.87,40.94,40.94,40.94,40.94,40.86,40.94,40.94,40.94,40.94],
+  				name: 'Internet',
+  				type: 'bar',
+  				marker: {
+  					color: 'DBF978'
+  				}
+			};
+			
+			var internetRose = {
+  				t: ['June', 'July', 'August', 'September', 'October', 'November', 
+  					'December', 'January', 'February', 'March', 'April', 'May'],
+  				r: [247.46,51.83,31.87,40.94,40.94,40.94,40.94,40.86,40.94,40.94,40.94,40.94],
+  				name: 'Internet',
+  				type: 'area',
+  				marker: {
+  					color: 'DBF978'
+  				}
+			};
+			
+			var foodStaples = {
+  				x: ['June', 'July', 'August', 'September', 'October', 'November', 
+  					'December', 'January', 'February', 'March', 'April', 'May'],
+  				y: [303.92,488.13,52.06,459.74,223.9,610.64,206.37,314.78,239.57,226.66,166.12,528.37],
+  				name: 'Food Staples',
+  				type: 'bar',
+  				marker: {
+  					color: '000000'
+  				}
+			};
+			
+			var foodStaplesRose = {
+  				t: ['June', 'July', 'August', 'September', 'October', 'November', 
+  					'December', 'January', 'February', 'March', 'April', 'May'],
+  				r: [303.92,488.13,52.06,459.74,223.9,610.64,206.37,314.78,239.57,226.66,166.12,528.37],
+  				name: 'Food Staples',
+  				type: 'area',
+  				marker: {
+  					color: '000000'
+  				}
+			};
+
+			var foodEatingOut = {
+  				x: ['June', 'July', 'August', 'September', 'October', 'November', 
+  					'December', 'January', 'February', 'March', 'April', 'May'],
+  				y: [85.36,35.04,5.19,25.76,86.19,139.03,95.5,224.68,50.98,63.39,105.68,212.81],
+  				name: 'Food Eating Out',
+  				type: 'bar',
+  				marker: {
+  					color: 'F2F7A6'
+  				}
+			};
+			
+			var foodEatingOutRose = {
+  				t: ['June', 'July', 'August', 'September', 'October', 'November', 
+  					'December', 'January', 'February', 'March', 'April', 'May'],
+  				r: [85.36,35.04,5.19,25.76,86.19,139.03,95.5,224.68,50.98,63.39,105.68,212.81],
+  				name: 'Food Eating Out',
+  				type: 'area',
+  				marker: {
+  					color: 'F2F7A6'
+  				}
+			};
+			
+			var travel = {
+  				x: ['June', 'July', 'August', 'September', 'October', 'November', 
+  					'December', 'January', 'February', 'March', 'April', 'May'],
+  				y: [118.72,311.96,200.78,249.5,183.98,155.46,141.56,398.94,190.26,144.4,178.75,255.8],
+  				name: 'Travel',
+  				type: 'bar',
+  				marker: {
+  					color: 'F7A6D7'
+  				}
+			};
+			
+			var travelRose = {
+  				t: ['June', 'July', 'August', 'September', 'October', 'November', 
+  					'December', 'January', 'February', 'March', 'April', 'May'],
+  				r: [118.72,311.96,200.78,249.5,183.98,155.46,141.56,398.94,190.26,144.4,178.75,255.8],
+  				name: 'Travel',
+  				type: 'area',
+  				marker: {
+  					color: 'F7A6D7'
+  				}
+			};
+
+			var presents = {
+  				x: ['June', 'July', 'August', 'September', 'October', 'November', 
+  					'December', 'January', 'February', 'March', 'April', 'May'],
+  				y: [219.11,86.98,58,0,0,33.97,154.47,0,0,36,22.07,0],
+  				name: 'Presents',
+  				type: 'bar',
+  				marker: {
+  					color: '2474B2'
+  				}
+			};
+			
+			var presentsRose = {
+  				t: ['June', 'July', 'August', 'September', 'October', 'November', 
+  					'December', 'January', 'February', 'March', 'April', 'May'],
+  				r: [219.11,86.98,58,0,0,33.97,154.47,0,0,36,22.07,0],
+  				name: 'Presents',
+  				type: 'area',
+  				marker: {
+  					color: '2474B2'
+  				}
+			};
+			
+			var miscellaneous = {
+  				x: ['June', 'July', 'August', 'September', 'October', 'November', 
+  					'December', 'January', 'February', 'March', 'April', 'May'],
+  				y: [509.18,240.03,66.1,378.83,146.29,85.16,177.47,203.23,141.07,206.93,142.22,208.64],
+  				name: 'Miscellaneous spending',
+  				type: 'bar',
+  				marker: {
+  					color: 'ECA12F'
+  				}
+			};
+
+			var miscellaneousRose = {
+  				t: ['June', 'July', 'August', 'September', 'October', 'November', 
+  					'December', 'January', 'February', 'March', 'April', 'May'],
+  				r: [509.18,240.03,66.1,378.83,146.29,85.16,177.47,203.23,141.07,206.93,142.22,208.64],
+  				name: 'Miscellaneous spending',
+  				type: 'area',
+  				marker: {
+  					color: 'ECA12F'
+  				}
+			};
+
+			var emergency = {
+  				x: ['June', 'July', 'August', 'September', 'October', 'November', 
+  					'December', 'January', 'February', 'March', 'April', 'May'],
+  				y: [0,40,556.63,30,23.46,137.6,100,109.58,0,0,0,0],
+  				name: 'Emergency',
+  				type: 'bar',
+  				marker: {
+  					color: 'CE3A89'
+  				}
+			};
+			
+			var emergencyRose = {
+  				t: ['June', 'July', 'August', 'September', 'October', 'November', 
+  					'December', 'January', 'February', 'March', 'April', 'May'],
+  				r: [0,40,556.63,30,23.46,137.6,100,109.58,0,0,0,0],
+  				name: 'Emergency',
+  				type: 'area',
+  				marker: {
+  					color: 'CE3A89'
+  				}
+			};
+			
+			var totalPerMonth = {
+  				x: ['June', 'July', 'August', 'September', 'October', 'November', 
+  					'December', 'January', 'February', 'March', 'April', 'May'],
+  				y: [2604.08,2885.23,2606.1,2810.64,2334.07,2916.06,2571.52,2947.65,2311.38,2383.67,2307.72,2875.71],
+  				name: 'Total per month',
+  				type: 'bar',
+  				marker: {
+  					color: 'C0C0C0'
+  				}
+			};
+			
+			var projectedTotal = {
+				x: ['June', 'July', 'August', 'September', 'October', 'November', 
+  					'December', 'January', 'February', 'March', 'April', 'May'],
+  				y: [2662,2733.09,2611.87,2828.31,2420.94,2694.04,2739.4,2729.51,2750,2750,2750,2750],
+  				name: 'Projected total per month',
+  				type: 'bar',
+  				marker: {
+  					color: 'C0C0C0'
+  				}
+			}
+
+			
+
+
+			var layout = {
+				barmode: 'group',
+				title: 'Spending over time',
+  				font:{
+    				family: 'Courier New'
+  				},
+  				//showlegend: false,
+  				xaxis: {
+   	 				tickangle: -45
+  				},
+  				yaxis: {
+    				zeroline: false,
+    				gridwidth: 2
+  				},
+  				bargap :0.1,
+  				autosize: true
+			};
+						
+			var layoutRose = {
+			  title: 'Rent throughout 2016-17',
+			  font: {size: 16},
+			  showlegend: false,
+			  radialaxis: {ticksuffix: "$"},
+			  orientation: 270,
+			  autosize: true,
+			};
+			var getLayout = function(layout, newTitle)
+			{
+				layout.title = newTitle;
+				return layout;
+			};
+			var getGraphType = function(graph, graphType)
+			{
+				graph.type = graphType;
+				return graph;
+			};
+
+			//var allInfo = [rent, rentInsurance, coned, internet, foodStaples, foodEatingOut, travel, presents, miscellaneous, emergency, totalPerMonth];
+			
+			var getMonthData = function(monthNumber, monthName, monthColor)
+			{
+				var month = {
+						
+						x: [rent['y'][monthNumber], rentInsurance['y'][monthNumber], coned['y'][monthNumber], 
+							internet['y'][monthNumber], foodStaples['y'][monthNumber], foodEatingOut['y'][monthNumber], 
+							travel['y'][monthNumber], presents['y'][monthNumber], miscellaneous['y'][monthNumber], 
+							emergency['y'][monthNumber], totalPerMonth['y'][monthNumber], projectedTotal['y'][monthNumber]],
+						y: ['Rent', 'Renter Insurance', 'ConEd', 'Internet', 'Food Staples', 'Food Eating Out',
+							'Travel', 'Presents', 'Miscellaneous', 'Emergency', 'Total','Projected Total'],
+						name: monthName + ' Spending',
+						type: 'bar',
+						orientation: 'h',
+						marker:
+						{
+							color: monthColor
+						},
+				}
+				return month;
+			};
+			
+			
+			//year 2 counts
+			var rent2 = {
+				x: ['June', 'July', 'August', 'September', 'October', 'November', 
+  					'December', 'January', 'February', 'March', 'April', 'May'],
+  				y: [1550,1550,1550,1550,1550,1550,1550,1550,1550,1550,1550,1550],
+  				name: 'Rent',
+  				type: 'bar',
+  				marker: {
+    				color: 'A9BBF7'
+  				}
+			}
+			
+			var rentInsurance2 = {
+  				x: ['June', 'July', 'August', 'September', 'October', 'November', 
+  					'December', 'January', 'February', 'March', 'April', 'May'],
+  				y: [23.47,23.47,23.47,23.47,23.47,23.47,23.47,23.47,23.47,23.47,23.47,23.46],
+  				name: "Renter's Insurance",
+  				type: 'bar',
+  				marker: {
+  					color: 'A9E0F7'
+  				}
+			};
+			
+			var coned2 = {
+  				x: ['June', 'July', 'August', 'September', 'October', 'November', 
+  					'December', 'January', 'February', 'March', 'April', 'May'],
+  				y: [86.42,0,0,0,0,0,0,0,0,0,0,0],
+  				name: 'ConEd',
+  				type: 'bar',
+  				marker: {
+  					color: 'A9F7BE'
+  				}
+			};
+			
+			var internet2 = {
+  				x: ['June', 'July', 'August', 'September', 'October', 'November', 
+  					'December', 'January', 'February', 'March', 'April', 'May'],
+  				y: [50.94,50.94,50.94,50.94,50.94,50.94,50.94,50.86,50.94,50.94,50.94,50.94],
+  				name: 'Internet',
+  				type: 'bar',
+  				marker: {
+  					color: 'DBF978'
+  				}
+			};
+			
+			var foodStaples2 = {
+  				x: ['June', 'July', 'August', 'September', 'October', 'November', 
+  					'December', 'January', 'February', 'March', 'April', 'May'],
+  				y: [275.14],
+  				name: 'Food Staples',
+  				type: 'bar',
+  				marker: {
+  					color: '000000'
+  				}
+			};
+			
+			var foodEatingOut2 = {
+  				x: ['June', 'July', 'August', 'September', 'October', 'November', 
+  					'December', 'January', 'February', 'March', 'April', 'May'],
+  				y: [74.34],
+  				name: 'Food Eating Out',
+  				type: 'bar',
+  				marker: {
+  					color: 'F2F7A6'
+  				}
+			};
+			
+			var travel2 = {
+  				x: ['June', 'July', 'August', 'September', 'October', 'November', 
+  					'December', 'January', 'February', 'March', 'April', 'May'],
+  				y: [329.25],
+  				name: 'Travel',
+  				type: 'bar',
+  				marker: {
+  					color: 'F7A6D7'
+  				}
+			};
+			
+			var presents2 = {
+  				x: ['June', 'July', 'August', 'September', 'October', 'November', 
+  					'December', 'January', 'February', 'March', 'April', 'May'],
+  				y: [0,7.48],
+  				name: 'Presents',
+  				type: 'bar',
+  				marker: {
+  					color: '2474B2'
+  				}
+			};
+			
+			var miscellaneous2 = {
+  				x: ['June', 'July', 'August', 'September', 'October', 'November', 
+  					'December', 'January', 'February', 'March', 'April', 'May'],
+  				y: [250.48],
+  				name: 'Miscellaneous spending',
+  				type: 'bar',
+  				marker: {
+  					color: 'ECA12F'
+  				}
+			};
+
+			var totalPerMonth2 = {
+  				x: ['June', 'July', 'August', 'September', 'October', 'November', 
+  					'December', 'January', 'February', 'March', 'April', 'May'],
+  				y: [2640.13],
+  				name: 'Total per month',
+  				type: 'bar',
+  				marker: {
+  					color: 'C0C0C0'
+  				}
+			};
+			
+			var projectedTotal2 = {
+				x: ['June', 'July', 'August', 'September', 'October', 'November', 
+  					'December', 'January', 'February', 'March', 'April', 'May'],
+  				y: [2750,2750],
+  				name: 'Projected total per month',
+  				type: 'bar',
+  				marker: {
+  					color: 'C0C0C0'
+  				}
+			}
+			
+			var getMonthDataYear2 = function(monthNumber, monthName, monthColor)
+			{
+				var month = {
+						
+						x: [rent2['y'][monthNumber], rentInsurance2['y'][monthNumber], coned2['y'][monthNumber], 
+							internet2['y'][monthNumber], foodStaples2['y'][monthNumber], foodEatingOut2['y'][monthNumber], 
+							travel2['y'][monthNumber], presents2['y'][monthNumber], miscellaneous2['y'][monthNumber], 
+							totalPerMonth2['y'][monthNumber], projectedTotal2['y'][monthNumber]],
+						y: ['Rent', 'Renter Insurance', 'ConEd', 'Internet', 'Food Staples', 'Food Eating Out',
+							'Travel', 'Presents', 'Miscellaneous', 'Total','Projected Total'],
+						name: monthName + ' Spending',
+						type: 'bar',
+						orientation: 'h',
+						marker:
+						{
+							color: monthColor
+						},
+				}
+				return month;
+			};
